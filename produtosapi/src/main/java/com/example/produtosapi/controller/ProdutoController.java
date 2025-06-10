add6a66d0b73681363bcb15e3cd47d43f95189d3
Accenture\Bruno Alves da Silva <bruno.alves.da.silva@accenture.com>
2025-06-10 11:06:21 -0300

    Até a aula 19

--- produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,59 @@
 package com.example.produtosapi.controller;
 
 import com.example.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
-    @PostMapping()
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
+        //
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        System.out.println("Id do Produto a ser buscado: " + id);
+        //Optional<Produto> produto = produtoRepository.findById(id);
+        //return produto.isPresent() ? produto.get() : null;
+
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        System.out.println("Id do Produto a ser deletado: " + id);
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable String id,@RequestBody Produto produto){
+        System.out.println("Id do Produto a ser atualizado: " + id);
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        System.out.println("Realizando a busca pelo Nome: " + nome);
+        return produtoRepository.findByNome(nome);
     }
 }
